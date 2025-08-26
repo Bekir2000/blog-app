e5ccb8576904a58e7351795ec91c96105f857c75
Ebubekir Popczyk <ebubekir.popczyk.de@gmail.com>
2025-08-26 16:54:17 +0200

    feat(post): implement CRUD logic in PostServiceImpl
    - Implemented draft fetching by author
    - Added createPost with category and tags
    - Added updatePost with category/tags update
    - Added deletePost with validation
    - Integrated reading time calculation

--- blog-backend/src/main/java/org/example/blogbackend/service/impl/PostServiceImpl.java
+++ blog-backend/src/main/java/org/example/blogbackend/service/impl/PostServiceImpl.java
@@ -3,9 +3,12 @@ package org.example.blogbackend.service.impl;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.example.blogbackend.mapper.PostMapper;
+import org.example.blogbackend.model.dto.requests.CreatePostRequest;
+import org.example.blogbackend.model.dto.requests.UpdatePostRequest;
 import org.example.blogbackend.model.entities.Category;
 import org.example.blogbackend.model.entities.Post;
 import org.example.blogbackend.model.entities.Tag;
+import org.example.blogbackend.model.entities.User;
 import org.example.blogbackend.model.enums.PostStatus;
 import org.example.blogbackend.repository.PostRepository;
 import org.example.blogbackend.service.CategoryService;
@@ -67,6 +70,66 @@ public class PostServiceImpl implements PostService {
         return postRepository.findAllByStatus(PostStatus.PUBLISHED);
     }
 
+    @Override
+    public List<Post> getDraftPosts(User user) {
+        return postRepository.findAllByAuthorAndStatus(user, PostStatus.DRAFT);
+    }
+
+    @Override
+    @Transactional
+    public Post createPost(User user, CreatePostRequest createPostRequest) {
+
+        Post postToCreate = postMapper.toEntity(createPostRequest);
+
+        postToCreate.setAuthor(user);
+        postToCreate.setReadingTime(calculateReadingTime(postToCreate.getContent()));
+
+        Category category = categoryService.getCategoryById(createPostRequest.getCategoryId());
+        postToCreate.setCategory(category);
+
+        Set<UUID> tagIds = createPostRequest.getTagIds();
+        List<Tag> tags = tagService.getTagsByIds(tagIds);
+        postToCreate.setTags(new HashSet<>(tags));
+
+        return postRepository.save(postToCreate);
+    }
+
+    @Override
+    @Transactional
+    public Post updatePost(UUID id, UpdatePostRequest updatePostRequest) {
+
+        Post existingPost = postRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Post not found with ID: " + id));
+
+        existingPost.setTitle(updatePostRequest.getTitle());
+        existingPost.setContent(updatePostRequest.getContent());
+        existingPost.setReadingTime(calculateReadingTime(existingPost.getContent()));
+        existingPost.setStatus(updatePostRequest.getStatus());
+
+        UUID updatePostRequestCategoryId = updatePostRequest.getCategoryId();
+        if(!existingPost.getCategory().getId().equals(updatePostRequestCategoryId)){
+            Category category = categoryService.getCategoryById(updatePostRequestCategoryId);
+            existingPost.setCategory(category);
+        }
+
+        Set<UUID> existingTagIds = existingPost
+                .getTags()
+                .stream()
+                .map(Tag::getId)
+                .collect(Collectors.toSet());
+        Set<UUID> updatePostRequestTagIds = updatePostRequest.getTagIds();
+        if(!existingTagIds.equals(updatePostRequestTagIds)){
+            List<Tag> tags = tagService.getTagsByIds(updatePostRequestTagIds);
+            existingPost.setTags(new HashSet<>(tags));
+        }
+        return postRepository.save(existingPost);
+    }
+
+    @Override
+    public void deletePost(UUID id) {
+        Post post = getPostById(id);
+        postRepository.delete(post);
+    }
 
     private Integer calculateReadingTime(String content) {
         if(content == null || content.isBlank()){
