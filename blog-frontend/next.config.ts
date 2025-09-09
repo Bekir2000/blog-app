97940d43ee5d032242ee5a2ef60ee815337ee895
Ebubekir Popczyk <ebubekir.popczyk.de@gmail.com>
2025-09-09 18:11:19 +0200

    chore(config): update next.config for remote images and sync deps

--- blog-frontend/next.config.ts
+++ blog-frontend/next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+
+  // To Do: delete after setting up api
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "miro.medium.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
